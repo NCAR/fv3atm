0014d235d2378a679c83f52c78b8924796ab1869
Dom Heinzeller <dom.heinzeller@noaa.gov>
2018-05-31 10:25:22 -0600

    ipd/IPD_driver.F90: add IPD_Interstitial to argument lists (may be reverted later, see note in file) and define IPD_finalize routine

--- ipd/IPD_driver.F90
+++ ipd/IPD_driver.F90
@@ -1,15 +1,30 @@
 module IPD_driver
 
+! DH* note - passing the interstitial data types to
+! IPD_step (and the explicit interfaces behind it,
+! time_vary_step, radiation_step1, physics_step1/2)
+! may be reverted once we no longer have to call
+! ccpp_physics_run from gfs_*driver.F90 *DH
+
   use IPD_typedefs,               only: IPD_kind_phys,     IPD_init_type,    &
                                         IPD_control_type,  IPD_data_type,    &
                                         IPD_diag_type,     IPD_restart_type, &
+#ifdef CCPP
+                                        IPD_interstitial_type, IPD_fastphys_type, &
                                         IPD_func0d_proc,   IPD_func1d_proc
+#else
+                                        IPD_func0d_proc,   IPD_func1d_proc
+#endif
 
   use physics_abstraction_layer,  only: initialize,        time_vary_step,   &
                                         radiation_step1,   physics_step1,    &
                                         physics_step2,                       &
+#ifdef CCPP
+                                        diagnostic_populate, restart_populate,&
+                                        finalize
+#else
                                         diagnostic_populate, restart_populate
-
+#endif
   implicit none
 
 !------------------------------------------------------!
@@ -31,6 +46,9 @@ module IPD_driver
 !rab  public IPD_radiation_step
 !rab  public IPD_physics_step1
 !rab  public IPD_physics_step2
+#ifdef CCPP
+  public IPD_finalize
+#endif
 
   CONTAINS
 !*******************************************************************************************
@@ -39,18 +57,30 @@ module IPD_driver
 !----------------
 !  IPD Initialize 
 !----------------
+#ifdef CCPP
+  subroutine IPD_initialize (IPD_Control, IPD_Data, IPD_Diag, IPD_Restart, IPD_Fastphys, IPD_Interstitial, IPD_init_parm)
+#else
   subroutine IPD_initialize (IPD_Control, IPD_Data, IPD_Diag, IPD_Restart, IPD_init_parm)
+#endif
     type(IPD_control_type), intent(inout) :: IPD_Control
     type(IPD_data_type),    intent(inout) :: IPD_Data(:)
     type(IPD_diag_type),    intent(inout) :: IPD_Diag(:)
     type(IPD_restart_type), intent(inout) :: IPD_Restart
+#ifdef CCPP
+    type(IPD_fastphys_type), intent(inout) :: IPD_Fastphys
+    type(IPD_interstitial_type), intent(inout) :: IPD_Interstitial(:)
+#endif
     type(IPD_init_type),    intent(in)    :: IPD_init_parm
 
     !--- initialize the physics suite
     call initialize (IPD_Control, IPD_Data(:)%Statein, IPD_Data(:)%Stateout,      &
                      IPD_Data(:)%Sfcprop, IPD_Data(:)%Coupling, IPD_Data(:)%Grid, &
                      IPD_Data(:)%Tbd, IPD_Data(:)%Cldprop, IPD_Data(:)%Radtend,   &
+#ifdef CCPP
+                     IPD_Data(:)%Intdiag, IPD_Fastphys, IPD_Interstitial(:), IPD_init_parm)
+#else
                      IPD_Data(:)%Intdiag, IPD_init_parm)
+#endif
 
 
     !--- populate/associate the Diag container elements
@@ -73,11 +103,18 @@ module IPD_driver
 !  IPD step
 !    runs the given routine/function pointed to by IPD_func 
 !----------------------------------------------------------
+#ifdef CCPP
+  subroutine IPD_step (IPD_Control, IPD_Data, IPD_Diag, IPD_Restart, IPD_Interstitial, IPD_func0d, IPD_func1d)
+#else
   subroutine IPD_step (IPD_Control, IPD_Data, IPD_Diag, IPD_Restart, IPD_func0d, IPD_func1d)
+#endif
     type(IPD_control_type),     intent(inout) :: IPD_Control
     type(IPD_data_type),        intent(inout) :: IPD_Data(:)
     type(IPD_diag_type),        intent(inout) :: IPD_Diag(:)
     type(IPD_restart_type),     intent(inout) :: IPD_Restart
+#ifdef CCPP
+    type(IPD_interstitial_type), intent(inout) :: IPD_Interstitial(:)
+#endif
     procedure(IPD_func0d_proc), intent(in), optional, pointer :: IPD_func0d
     procedure(IPD_func1d_proc), intent(in), optional, pointer :: IPD_func1d
 
@@ -85,14 +122,32 @@ module IPD_driver
       call IPD_func0d (IPD_Control, IPD_Data(1)%Statein, IPD_Data(1)%Stateout,        &
                        IPD_Data(1)%Sfcprop, IPD_Data(1)%Coupling, IPD_Data(1)%Grid, &
                        IPD_Data(1)%Tbd, IPD_Data(1)%Cldprop, IPD_Data(1)%Radtend,   &
+#ifdef CCPP
+                       IPD_Data(1)%Intdiag, IPD_Interstitial) ! need to pass interstitial data for all threads
+#else
                        IPD_Data(1)%Intdiag)
+#endif
     else
       call IPD_func1d (IPD_Control, IPD_Data(:)%Statein, IPD_Data(:)%Stateout,      &
                        IPD_Data(:)%Sfcprop, IPD_Data(:)%Coupling, IPD_Data(:)%Grid, &
                        IPD_Data(:)%Tbd, IPD_Data(:)%Cldprop, IPD_Data(:)%Radtend,   &
+#ifdef CCPP
+                       IPD_Data(:)%Intdiag, IPD_Interstitial) ! need to pass interstitial data for all threads
+#else
                        IPD_Data(:)%Intdiag)
+#endif
     endif
 
   end subroutine IPD_step
+
+#ifdef CCPP
+!----------------
+!  IPD finalize
+!----------------
+  subroutine IPD_finalize ()
+    !--- finalize the physics suite
+    call finalize ()
+  end subroutine IPD_finalize
+#endif
 
 end module IPD_driver
