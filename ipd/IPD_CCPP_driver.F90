b3e06da96e8dfcf9c99382084ed4b21aad8aa447
Dom Heinzeller <dom.heinzeller@noaa.gov>
2018-09-07 22:39:20 -0600

    ipd/IPD_CCPP_driver.F90: minimal changes to import ccpp_physics_{init,run,finalize} from different module for static build

--- ipd/IPD_CCPP_driver.F90
+++ ipd/IPD_CCPP_driver.F90
@@ -8,11 +8,22 @@ module IPD_CCPP_driver
   use ccpp_api,           only: ccpp_t,                              &
                                 ccpp_init,                           &
                                 ccpp_finalize,                       &
+#ifdef STATIC
+! For static builds, the ccpp_physics_{init,run,finalize} calls
+! are imported via ccpp_modules_static_slow_physics.inc. These are not
+! pointing to code in the CCPP framework, but to auto-generated
+! ccpp_suite_cap and ccpp_group_*_cap modules behind a ccpp_static_api
+#else
                                 ccpp_physics_init,                   &
                                 ccpp_physics_run,                    &
                                 ccpp_physics_finalize,               &
+#endif
                                 ccpp_field_add,                      &
                                 ccpp_initialized
+
+#ifdef STATIC
+#include "ccpp_modules_static_slow_physics.inc"
+#endif
 
   use CCPP_data,          only: cdata_tile,                          &
                                 cdata_domain,                        &
