bcfed5b20a6b18f5e9ad96b16763889a0fb0c4b6
Qingfu Liu <Qingfu.Liu@noaa.gov>
2019-08-27 11:17:39 +0000

    add MYJ surface layer scheme and MYJ PBL scheme to CCPP

--- gfsphysics/GFS_layer/GFS_typedefs.F90
+++ gfsphysics/GFS_layer/GFS_typedefs.F90
@@ -369,6 +369,7 @@ module GFS_typedefs
     real (kind=kind_phys), pointer :: chs2(:)          => null()  !exch coeff for heat at 2m
     real (kind=kind_phys), pointer :: cqs2(:)          => null()  !exch coeff for moisture at 2m
     real (kind=kind_phys), pointer :: lh(:)            => null()  !latent heating at the surface
+
 #endif
 
     contains
@@ -830,6 +831,9 @@ module GFS_typedefs
     integer              :: bl_mynn_mixqt      !< flag to mix total water or individual species
     integer              :: icloud_bl          !< flag for coupling sgs clouds to radiation
     ! *DH
+    ! MYJ switches
+    logical              :: do_myjsfc          !< flag for MYJ surface layer scheme
+    logical              :: do_myjpbl          !< flag for MYJ PBL scheme
 #endif
 
 !--- Rayleigh friction
@@ -1163,6 +1167,21 @@ module GFS_typedefs
     real (kind=kind_phys), pointer :: tsq        (:,:)   => null()  !
     real (kind=kind_phys), pointer :: qsq        (:,:)   => null()  !
     real (kind=kind_phys), pointer :: cov        (:,:)   => null()  !
+
+    !--- MYJ schemes saved variables (from previous time step)
+    real (kind=kind_phys), pointer :: phy_myj_qsfc(:)    => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_thz0(:)    => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_qz0(:)     => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_uz0(:)     => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_vz0(:)     => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_z0base(:)  => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_akhs(:)    => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_akms(:)    => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_chkqlm(:)  => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_elflx(:)   => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_a1u(:)     => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_a1t(:)     => null()  ! 
+    real (kind=kind_phys), pointer :: phy_myj_a1q(:)     => null()  ! 
 #endif
 
     contains
@@ -2778,6 +2797,8 @@ module GFS_typedefs
     integer              :: bl_mynn_mixqt     = 0
     integer              :: icloud_bl         = 1
     ! *DH
+    logical              :: do_myjsfc         = .false.               !< flag for MYJ surface layer scheme
+    logical              :: do_myjpbl         = .false.               !< flag for MYJ PBL scheme
 #endif
     integer              :: nmtvr          = 14                       !< number of topographic variables such as variance etc
                                                                       !< used in the GWD parameterization
@@ -2961,6 +2982,7 @@ module GFS_typedefs
                                bl_mynn_edmf_tke, bl_mynn_edmf_part, bl_mynn_cloudmix,       &
                                bl_mynn_mixqt, icloud_bl, bl_mynn_tkeadvect,                 &
                                ! *DH
+                               do_myjsfc, do_myjpbl,                                           &
 #endif
                                h2o_phys, pdfcld, shcnvcw, redrag, hybedmf, satmedmf,        &
                                shinhong, do_ysu, dspheat, dspheat, lheatstrg, cnvcld,       &
@@ -3359,6 +3381,8 @@ module GFS_typedefs
     Model%grav_settling     = grav_settling
     Model%icloud_bl         = icloud_bl
     ! *DH
+    Model%do_myjsfc         = do_myjsfc
+    Model%do_myjpbl         = do_myjpbl
 #endif
 
 !--- Rayleigh friction
@@ -3812,6 +3836,8 @@ module GFS_typedefs
 #ifdef CCPP
       elseif (Model%do_mynnedmf) then
         print *,' MYNN PBL scheme used'
+      elseif (Model%do_myjpbl)then
+        print *,' MYJ PBL scheme used'
 #endif
       endif
       if (.not. Model%shal_cnv) then
@@ -4287,6 +4313,8 @@ module GFS_typedefs
 #ifdef CCPP
       print *, ' do_mynnedmf       : ', Model%do_mynnedmf
       print *, ' do_mynnsfclay     : ', Model%do_mynnsfclay
+      print *, ' do_myjsfc         : ', Model%do_myjsfc
+      print *, ' do_myjpbl         : ', Model%do_myjpbl
 #endif
       print *, ' '
       print *, 'Rayleigh friction'
@@ -4645,6 +4673,38 @@ module GFS_typedefs
        Tbd%tsq           = clear_val
        Tbd%qsq           = clear_val
        Tbd%cov           = clear_val
+    end if
+    
+    ! MYJ variables
+    if (Model%do_myjsfc.or.Model%do_myjpbl) then
+       !print*,"Allocating all MYJ surface variables:"
+       allocate (Tbd%phy_myj_qsfc   (IM))
+       allocate (Tbd%phy_myj_thz0   (IM)) 
+       allocate (Tbd%phy_myj_qz0    (IM)) 
+       allocate (Tbd%phy_myj_uz0    (IM)) 
+       allocate (Tbd%phy_myj_vz0    (IM)) 
+       allocate (Tbd%phy_myj_z0base (IM)) 
+       allocate (Tbd%phy_myj_akhs   (IM)) 
+       allocate (Tbd%phy_myj_akms   (IM)) 
+       allocate (Tbd%phy_myj_chkqlm (IM)) 
+       allocate (Tbd%phy_myj_elflx  (IM)) 
+       allocate (Tbd%phy_myj_a1u    (IM)) 
+       allocate (Tbd%phy_myj_a1t    (IM)) 
+       allocate (Tbd%phy_myj_a1q    (IM))
+       !print*,"Allocating all MYJ schemes variables:"
+       Tbd%phy_myj_qsfc   = clear_val 
+       Tbd%phy_myj_thz0   = clear_val 
+       Tbd%phy_myj_qz0    = clear_val 
+       Tbd%phy_myj_uz0    = clear_val 
+       Tbd%phy_myj_vz0    = clear_val 
+       Tbd%phy_myj_z0base = clear_val 
+       Tbd%phy_myj_akhs   = clear_val 
+       Tbd%phy_myj_akms   = clear_val 
+       Tbd%phy_myj_chkqlm = clear_val 
+       Tbd%phy_myj_elflx  = clear_val 
+       Tbd%phy_myj_a1u    = clear_val 
+       Tbd%phy_myj_a1t    = clear_val 
+       Tbd%phy_myj_a1q    = clear_val 
     end if
 #endif
 
