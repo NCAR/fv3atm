daab7457cf08c205c5016c8ec7865fbb08b8495f
Dom Heinzeller <dom.heinzeller@noaa.gov>
2019-09-13 16:29:43 -0600

    gfsphysics/GFS_layer/GFS_typedefs.{F90,meta}: introduce lower bound variable for snow-related arrays in NoahMP, use in metadata table

--- gfsphysics/GFS_layer/GFS_typedefs.F90
+++ gfsphysics/GFS_layer/GFS_typedefs.F90
@@ -718,6 +718,7 @@ module GFS_typedefs
 #ifdef CCPP
     integer              :: lsoil_lsm       !< number of soil layers internal to land surface model
     integer              :: lsnow_lsm       !< maximum number of snow layers internal to land surface model
+    integer              :: lsnow_lsm_lbound!< lower bound for snow arrays, depending on lsnow_lsm
 #endif
     integer              :: ivegsrc         !< ivegsrc = 0   => USGS, 
                                             !< ivegsrc = 1   => IGBP (20 category)
@@ -2161,11 +2162,11 @@ module GFS_typedefs
     allocate (Sfcprop%deeprechxy (IM))
     allocate (Sfcprop%rechxy    (IM))
 #ifdef CCPP
-    allocate (Sfcprop%snicexy    (IM, -Model%lsnow_lsm+1:0))
-    allocate (Sfcprop%snliqxy    (IM, -Model%lsnow_lsm+1:0))
-    allocate (Sfcprop%tsnoxy     (IM, -Model%lsnow_lsm+1:0))
+    allocate (Sfcprop%snicexy    (IM, Model%lsnow_lsm_lbound:0))
+    allocate (Sfcprop%snliqxy    (IM, Model%lsnow_lsm_lbound:0))
+    allocate (Sfcprop%tsnoxy     (IM, Model%lsnow_lsm_lbound:0))
     allocate (Sfcprop%smoiseq    (IM, Model%lsoil_lsm))
-    allocate (Sfcprop%zsnsoxy    (IM, -Model%lsnow_lsm+1:Model%lsoil_lsm))
+    allocate (Sfcprop%zsnsoxy    (IM, Model%lsnow_lsm_lbound:Model%lsoil_lsm))
 #else
     allocate (Sfcprop%snicexy    (IM,-2:0))
     allocate (Sfcprop%snliqxy    (IM,-2:0))
@@ -3311,7 +3312,9 @@ module GFS_typedefs
       write(0,*) 'Logic error: NoahMP expects the maximum number of snow layers to be exactly 3 (see sfc_noahmp_drv.f)'
       stop
     else
-      Model%lsnow_lsm      = lsnow_lsm
+      Model%lsnow_lsm        = lsnow_lsm
+      ! Set lower bound for LSM model, runs from negative (above surface) to surface (zero)
+      Model%lsnow_lsm_lbound = -Model%lsnow_lsm+1
     end if
 #endif
     Model%ivegsrc          = ivegsrc
